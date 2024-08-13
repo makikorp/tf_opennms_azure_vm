039fdd584ced9806e678b0362035a80c1bf44e1e
makikorp <36685794+makikorp@users.noreply.github.com>
2024-08-13 16:21:49 -0700

    Update compute.tf

--- compute.tf
+++ compute.tf
@@ -1,12 +1,12 @@
 # Generate random text for a unique storage account name
-resource "random_id" "random_id" {
-  keepers = {
-    # Generate a new ID only when a new resource group is defined
-    resource_group = azurerm_resource_group.rg.name
-  }
-
-  byte_length = 8
-}
+#resource "random_id" "random_id" {
+#  keepers = {
+#    # Generate a new ID only when a new resource group is defined
+#    resource_group = azurerm_resource_group.rg.name
+#  }
+#
+#  byte_length = 8
+#}
 
 # Create storage account for boot diagnostics
 resource "azurerm_storage_account" "my_storage_account" {
