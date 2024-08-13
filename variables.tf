31879750a079c3e62aeb5c86bf06b9766d03fa17
makikorp <36685794+makikorp@users.noreply.github.com>
2024-08-12 19:46:04 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,14 +6,14 @@ variable "resource_group_location" {
 
 variable "resource_group_name_prefix" {
   type        = string
-  default     = "eric_tf_opennms_rg"
+  default     = "<yourname>_tf_opennms_rg"
   description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
 }
 
 variable "username" {
   type        = string
   description = "The username for the local account that will be created on the new VM."
-  default     = "eric"
+  default     = "<username>"
 }
 
 variable "key_name" {
